870021864060f4c3cabd0280a9bb228d88ec0d14
yusuke akiyama <akiyama@hakoreco.com>
2019-06-25 09:59:12 +0900

    init

---
+++ src/vue/App.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  h1.tomato {{ greeting }} from {{ name }}
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      greeting: "Hello",
+      name: "Vue.js"
+    };
+  }
+};
+</script>
+
+<style lang="stylus">
+.tomato
+  color: tomato
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/main.ts",
+  // ファイルの出力設定
+  output: {
+    //  出力ファイルのディレクトリ名
+    path: `${__dirname}/dist`,
+    // 出力ファイル名
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      // this will apply to both plain `.js` files
+      // AND `<script>` blocks in `.vue` files
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      // this will apply to both plain `.css` files
+      // AND `<style>` blocks in `.vue` files
+      {
+        test: /\.styl(us)?$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: { appendTsSuffixTo: [/\.vue$/] }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-plain-loader'
+      }
+    ]
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ]
+}
